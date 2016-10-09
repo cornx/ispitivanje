a86b790021485959037fe98ad59fdc1f997ee2e0
cornx <exit>
2016-10-09 21:06:53 +0200

    dodan close na modal

--- js/ucenici.js
+++
@@ -1,4 +0,0 @@
-var osnoveTurizmaImena = ["Danijel", "Denis", "Celine", "Karlo", "Ivan"];
-var organizacijaImena = ["Luka","Petar","Dora","Meteo","Hrvoje","Iva"];
-var marketingImena = ["Damir", "Mateo", "Antonio", "Josip", "Marija", "sara", "Nikolina", "Petra"];
-var testImena = ["Učenik 1","Učenik 2","Učenik 3","Učenik 4","Učenik 5"];
